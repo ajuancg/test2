8b1e5163fc7a5a952708ba0c9eac5cea519655dd
JCA <ajuan.cg@gmail.com>
2021-10-21 11:49:03 -0300

    567

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,21 @@
-// Step 1: Import React
-import * as React from 'react'
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-// Step 2: Define your component
-const AboutPage = () => {
-  return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
-  )
-}
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>About</h1>
+    <p>Now go build something bad.</p>
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </p>
+  </Layout>
+)
 
 // Step 3: Export your component
 export default AboutPage
